9184ee7879bfcd6350ef2e0032a27bd06ed7c877
billzichos <billzichos@hotmail.com>
2015-09-11 15:59:53 -0400

    download r

--- download.R
+++ download.R
@@ -8,3 +8,36 @@ downloadKaggle <- function(competitionName, fileName) {
                 quiet = FALSE
         )
 }
+
+downloadSingleKaggleZip <- function(competitionName, zipFileName, fileName) {
+        # Use if zip file contains a single file.
+        kaggleUrl <- "https://www.kaggle.com/c/"
+        download.file(
+                paste(
+                        kaggleUrl,competitionName,'/download/',zipFileName,sep=""
+                ),
+                paste(getwd(),"\\",zipFileName,sep=""),
+                quiet = FALSE
+        )
+        
+        unzip(paste(getwd(),"\\",zipFileName,sep=""), fileName, exdir = getwd())
+        
+        unlink(paste(getwd(), "/", zipFileName,sep = ""))
+}
+
+
+downloadMultKaggleZip <- function(competitionName, zipFileName, newFolderName = "") {
+        # Use if zip file contains multiple files.
+        kaggleUrl <- "https://www.kaggle.com/c/"
+        download.file(
+                paste(
+                        kaggleUrl,competitionName,'/download/',zipFileName,sep=""
+                ),
+                paste(getwd(),"\\",zipFileName,sep=""),
+                quiet = FALSE
+        )
+        
+        unzip(paste(getwd(),"\\",zipFileName,sep=""), exdir = paste(getwd(), newFolderName, sep = ""))
+        
+        unlink(paste(getwd(), "/", zipFileName,sep = ""))
+}
